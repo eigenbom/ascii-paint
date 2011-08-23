79a95e45547f8dfc2f7e8b652fbb598ffeb31bb7
ben <ben@localhost>
2011-08-23 16:23:53 +1000

    Cleaned Up!

--- build-run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-clear
-export LD_LIBRARY_PATH=.:${LD_LIBRARY_PATH}
-scons -Q && ./ascii-paint
--- build.bat
+++
@@ -1,2 +0,0 @@
-g++ *.cpp -L. -ltcod-mingw -ltcod-gui-mingw -Iinclude -s -o ascii-paint.exe
-pause
